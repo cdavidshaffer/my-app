7af5ab42f41e4240d7fa7adbd848c14b4cdcc53d
C. David Shaffer <cdshaffer@acm.org>
2022-06-24 13:58:10 -0400

    Got cowsay working!

---
+++ src/main/java/edu/au/cpsc/App.java
@@ -0,0 +1,15 @@
+package edu.au.cpsc;
+
+import com.github.ricksbrown.cowsay.Cowsay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(Cowsay.say( new String[] {"-f", "dragon", "Hello World!"} ));
+    }
+}
